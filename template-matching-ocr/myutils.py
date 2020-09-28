7fcc4edbbc44f316b805e3247ca2d7c2b6c9e180
congcong-zjc <57975717+congcong-zjc@users.noreply.github.com>
2020-09-28 16:36:17 +0800

    信用卡数字识别

---
+++ template-matching-ocr/myutils.py
@@ -0,0 +1,29 @@
+import cv2
+
+def sort_contours(cnts, method="left-to-right"):
+    reverse = False
+    i = 0
+
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]  # 计算其外接矩形，用一个最小的矩形，把找到的形状包起来x,y,h,w；boundingBoxes不是一个值而是一个元组，元组中包含四个值x,y,h,w
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key=lambda b: b[1][i], reverse=reverse))
+
+    return cnts, boundingBoxes  #返回一个排序完的轮廓cnts
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+    resized = cv2.resize(image, dim, interpolation=inter)
+    return resized
